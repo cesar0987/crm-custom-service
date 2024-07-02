b9ce064afba27963b482edbb6e565be33192655d
cesar0987 <cesarbenitez46@gmail.com>
2024-07-02 16:47:28 -0400

    Changes

--- server/models/supliers.models.js
+++ server/models/supliers.models.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const supliersSchema = new mongoose.Schema({
-<<<<<<< HEAD
   name: {
     type: String,
     required: [true, "Name is required"],
@@ -32,44 +31,8 @@ const supliersSchema = new mongoose.Schema({
   sitioWeb: {
     type: String,
   },
-=======
-    name: {
-        type: String,
-        required: [true, "Name is required"],
-        minlength: [3, "Name must be at least 3 characters long"],
-    },
-    ruc: {
-        type: String,
-        required: [true, "RUC is required"],
-    },
-    phone: {
-        type: String, // Assuming phone number is stored as a string
-        required: [true, "Phone is required"],
-    },
-    address: {
-        type: String,
-        required: [true, "Address is required"],
-        minlength: [5, "Address must be at least 5 characters long"],
-    },
-    mail: {
-        type: String,
-        required: [true, "Mail is required"],
-        match: [/.+\@.+\..+/, "Please fill a valid email address"],
-    },
-    postalCode: {
-        type: String, // Assuming postal code is stored as a string
-        required: [true, "Postal code is required"],
-    },
-    sitioWep: {
-        type: String,
-    },
->>>>>>> ca51016 (form_Supliers)
 });
 
 const Supliers = mongoose.model("Supliers", supliersSchema);
 
 module.exports = Supliers;
-<<<<<<< HEAD
-=======
-
->>>>>>> ca51016 (form_Supliers)
