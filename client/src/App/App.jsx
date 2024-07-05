d6b73615d816624c1e7790d01fa6163df2f23e7c
Nelson-Sosa <ns5389188@gmail.com>
2024-07-05 18:50:00 -0400

    Arreglos a Supliers

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -55,6 +55,7 @@ const App = () => {
             <Route path="/help" element={<Help />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="*" element={<Dashboard />} />
+            <Route path="/actualizar/supliers/:id" element={<SupliersEdit />} />
           </Route>
 
         </Routes>
