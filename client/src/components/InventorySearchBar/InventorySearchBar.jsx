08d337ebd4d71cb1c4c42ab323f43a90a6f09aee
Nelson-Sosa <ns5389188@gmail.com>
2024-07-09 19:35:00 -0400

    btn Add Product

--- client/src/components/InventorySearchBar/InventorySearchBar.jsx
+++ client/src/components/InventorySearchBar/InventorySearchBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./InventorySearchBar.css";
-
+import { Link } from "react-router-dom";
 export const InventorySearchBar = () => {
   return (
     <div className="searchBar">
@@ -47,13 +47,14 @@ export const InventorySearchBar = () => {
             </button>
           </div>
         </form>
-
+        <Link to="/addProduct">
         <button
           type="button"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           New Product
         </button>
+        </Link>
       </div>
     </div>
   );
